d1cd8eea3f3f69446c7fbdc1393c45d2daac4151
Adam Nguyen <adanguyen814@gmail.com>
2023-05-02 19:34:13 -0400

    adding tests

---
+++ index.test.ts
@@ -0,0 +1,5 @@
+describe('my beverage', () => {
+    test('is delicious', () => {
+        (3===3);
+    });
+  });
--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
-window.onscroll = function () { myFunction(), myFunction2() };
+window.onscroll = function () { myFunction(), myFunction2(); };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById('myBar').style.width = `${scrolled}%`;
 }
 
 function myFunction2() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add('sticky');
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky');
   }
 }
-var p = 0;
-document.getElementById("myBtn").addEventListener("click", function () {
+let p = 0;
+document.getElementById('myBtn').addEventListener('click', () => {
   p++;
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-  var x = document.getElementsByClassName("title");
-  var m = document.getElementsByClassName("text1");
-  var i;
+  const element = document.body;
+  element.classList.toggle('dark-mode');
+  const x = document.getElementsByClassName('title');
+  const m = document.getElementsByClassName('text1');
+  let i;
   if (p % 2 == 1) {
-
-    document.getElementById("myBtn").className = "button is-black";
-    document.getElementById("myBtn").innerHTML = "Light Mode";
+    document.getElementById('myBtn').className = 'button is-black';
+    document.getElementById('myBtn').innerHTML = 'Light Mode';
     for (i = 0; i < x.length; i++) {
-      x[i].style.color = "purple";
+      x[i].style.color = 'purple';
     }
     for (i = 0; i < m.length; i++) {
-      m[i].style.color = "purple";
+      m[i].style.color = 'purple';
     }
   } else {
-    document.getElementById("myBtn").className = "button is-white";
-    document.getElementById("myBtn").innerHTML = "Dark Mode";
+    document.getElementById('myBtn').className = 'button is-white';
+    document.getElementById('myBtn').innerHTML = 'Dark Mode';
 
     for (i = 0; i < x.length; i++) {
-      x[i].style.color = "#363636";
+      x[i].style.color = '#363636';
     }
     for (i = 0; i < m.length; i++) {
-      m[i].style.color = "#363636";
+      m[i].style.color = '#363636';
     }
   }
-
 });
